d15fa93a847356d5c404c8c682940f9cb438d3a5
leadlost <eason.yuchendai@gmail.com>
2022-09-04 18:35:01 -0700

    Add enemy; add die tip

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,45 +5,70 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float speed=5.0f;
-
+    private bool is_alive = true;
+    private static GameObject die_tip;
     // Start is called before the first frame update
     void Start()
     {
-        
+        die_tip = GameObject.Find("Die");
+        die_tip.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.LeftArrow)){
-            transform.Translate(-speed * Time.deltaTime, 0, 0);
+        if(Input.GetKeyDown(KeyCode.R)){
+            respawn();
         }
-        if(Input.GetKey(KeyCode.RightArrow)){
-            transform.Translate(speed * Time.deltaTime, 0, 0);
+        if (is_alive){
+            die_tip.SetActive(false);
+            if(Input.GetKey(KeyCode.LeftArrow)){
+                transform.Translate(-speed * Time.deltaTime, 0, 0);
+            }
+            if(Input.GetKey(KeyCode.RightArrow)){
+                transform.Translate(speed * Time.deltaTime, 0, 0);
+            }
+            if(Input.GetKey(KeyCode.UpArrow)){
+                transform.Translate(0, speed * Time.deltaTime, 0);
+            }
+            if(Input.GetKey(KeyCode.DownArrow)){
+                transform.Translate(0, -speed * Time.deltaTime, 0);
+            }
         }
-        if(Input.GetKey(KeyCode.UpArrow)){
-            transform.Translate(0, speed * Time.deltaTime, 0);
+        else{
+            die_tip.SetActive(true);
         }
-        if(Input.GetKey(KeyCode.DownArrow)){
-            transform.Translate(0, -speed * Time.deltaTime, 0);
-        }        
+    }
+
+    public void die(){
+        is_alive = false;
+    }
+
+    public void respawn(){
+        is_alive = true;
+        float step = speed * Time.deltaTime;
+        transform.localPosition = new Vector3(-10, 0, 0);
     }
 
     private void OnCollisionEnter2D(Collision2D collision){
         if(collision.gameObject.tag == "Walls"){
             Debug.Log("hit");
-            if(Input.GetKey(KeyCode.LeftArrow)){
-                transform.Translate(speed * Time.deltaTime, 0, 0);
-            }
-            if(Input.GetKey(KeyCode.RightArrow)){
-                transform.Translate(-speed * Time.deltaTime, 0, 0);
-            }
-            if(Input.GetKey(KeyCode.UpArrow)){
-                transform.Translate(0, -speed * Time.deltaTime, 0);
-            }
-            if(Input.GetKey(KeyCode.DownArrow)){
-                transform.Translate(0, speed * Time.deltaTime, 0);
-            }  
+            // if(Input.GetKey(KeyCode.LeftArrow)){
+            //     transform.Translate(speed * Time.deltaTime, 0, 0);
+            // }
+            // if(Input.GetKey(KeyCode.RightArrow)){
+            //     transform.Translate(-speed * Time.deltaTime, 0, 0);
+            // }
+            // if(Input.GetKey(KeyCode.UpArrow)){
+            //     transform.Translate(0, -speed * Time.deltaTime, 0);
+            // }
+            // if(Input.GetKey(KeyCode.DownArrow)){
+            //     transform.Translate(0, speed * Time.deltaTime, 0);
+            // }  
+        }
+        else if(collision.gameObject.tag == "Enemy"){
+            Debug.Log("Collid Enemy");
+            die();
         }
     }
 
